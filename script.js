69eaac8ea0d95ef216187c6567df6d412b0a5f56
Nikos Andreopoulos <andreopoulos.b.nikolaos@gmail.com>
2024-02-05 15:29:28 +0200

    Fix bug of highscore

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-'use strict';
-document.querySelector('.check').onclick = checkNumber;
-document.querySelector('.again').onclick = reset;
+"use strict";
+document.querySelector(".check").onclick = checkNumber;
+document.querySelector(".again").onclick = reset;
 
-const score = document.querySelector('.score');
-const highscore = document.querySelector('.highscore');
-const message = document.querySelector('.message');
-const background = document.querySelector('body').style;
+const score = document.querySelector(".score");
+const highscore = document.querySelector(".highscore");
+const message = document.querySelector(".message");
+const background = document.querySelector("body").style;
 
 let guessNumber;
 guessNumber = reset();
 
 function checkNumber() {
-  const guessValue = document.querySelector('.guess').value;
+  const guessValue = document.querySelector(".guess").value;
   if (guessValue != guessNumber) {
     if (score.innerHTML == 1) {
       score.innerHTML = 0;
-      message.innerHTML = 'You lost :(';
-      background.backgroundColor = '#ee0707';
+      message.innerHTML = "You lost :(";
+      background.backgroundColor = "#ee0707";
       setTimeout(reset, 1500);
     } else if (score.innerHTML > 1) {
       score.innerHTML--;
-      message.innerHTML = 'Guess Again..';
+      message.innerHTML = "Guess Again..";
     }
   } else {
-    message.innerHTML = 'Correct Number!';
-    background.backgroundColor = '#60b347';
+    message.innerHTML = "Correct Number!";
+    background.backgroundColor = "#60b347";
     if (highscore.innerHTML < score.innerHTML) {
       highscore.innerHTML = score.innerHTML;
     }
@@ -36,7 +36,8 @@ function checkNumber() {
 function reset() {
   guessNumber = Math.floor(Math.random() * 20) + 1;
   score.innerHTML = 20;
-  background.backgroundColor = '#222';
-  message.innerHTML = 'Start guessing...';
+  background.backgroundColor = "#222";
+  message.innerHTML = "Start guessing...";
+  console.log(guessNumber);
   return guessNumber;
 }
